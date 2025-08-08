1eca08a1ad84fe2f5e7e4042eb7826ccdaa75b3e
Jeremy Andrews <jeremy@tag1consulting.com>
2025-08-08 19:34:16 +0200

    filter AND instead of OR

--- Argus/ArticleOperations.swift
+++ Argus/ArticleOperations.swift
@@ -627,11 +627,11 @@ final class ArticleOperations {
         let result: Bool
         switch filter {
         case "Fair+":
-            // Show articles with sourcesQuality ≥ 2 OR argumentQuality ≥ 2
-            result = sourcesQuality >= 2 || argumentQuality >= 2
+            // Show articles with sourcesQuality ≥ 2 AND argumentQuality ≥ 2
+            result = sourcesQuality >= 2 && argumentQuality >= 2
         case "Good+":
-            // Show articles with sourcesQuality ≥ 3 OR argumentQuality ≥ 3
-            result = sourcesQuality >= 3 || argumentQuality >= 3
+            // Show articles with sourcesQuality ≥ 3 AND argumentQuality ≥ 3
+            result = sourcesQuality >= 3 && argumentQuality >= 3
         default: // "All"
             result = true
         }
